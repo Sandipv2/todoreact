8ed316719fdc7e5e383800ae7443ff300a253094
sandipv2 <sandiipvarma@gmail.com>
2024-08-23 07:55:13 +0530

    github link add in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { FaGithub } from 'react-icons/fa'
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="fixed w-full">
-        <ul className="bg-stone-950 flex justify-around p-2 items-center">
-            <li className='text-4xl cursor-pointer'>Todo</li>
-            <li className="text-3xl hover:opacity-70 duration-300 cursor-pointer"><FaGithub/></li>
-        </ul>
+      <ul className="bg-stone-950 flex justify-around p-2 items-center">
+        <li className="text-4xl cursor-pointer">Todo</li>
+        <a href="https://github.com/Sandipv2/todoreact" target="_blank">
+          <li className="text-3xl hover:opacity-70 duration-300 cursor-pointer">
+            <FaGithub />
+          </li>
+        </a>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
